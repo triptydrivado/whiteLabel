8059b28ea4aef74a593e65f156660a9bba13ec36
Tripty Drivado <techsupport8@drivado.com>
2025-05-19 12:58:29 +0530

    changes

--- src/pages/search-results/components/journey-details/pax-count.tsx
+++ src/pages/search-results/components/journey-details/pax-count.tsx
@@ -9,7 +9,6 @@ type PaxCounterProps = {
 export default function PaxCounter({ paxCount }: PaxCounterProps) {
   return (
     <div className="relative">
-      {/* Trigger Section */}
       <div className="group flex max-w-[120px] items-center gap-x-3 rounded-lg bg-[#F5F6FA] p-[6px] transition-colors duration-300 xl:p-[10px]">
         <>
           <CapacityIcon className="h-3 min-h-3 w-3 min-w-3 flex-shrink-0 text-[var(--brand-theme-color)] transition-colors duration-300 lg:h-5 lg:w-5 2xl:h-[22px] 2xl:w-[22px]" />
