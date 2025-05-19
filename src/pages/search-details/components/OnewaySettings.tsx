119ebfa9a917e4a5c817c214fae8991eefc34fca
Tripty Drivado <techsupport8@drivado.com>
2025-05-19 11:23:38 +0530

    changes

--- src/pages/search-details/components/OnewaySettings.tsx
+++ src/pages/search-details/components/OnewaySettings.tsx
@@ -14,7 +14,7 @@ const OnewaySettings = () => {
   const bookingData = JSON.parse(bookngForm || "{}");
 
   return (
-    <div className="flex items-center justify-center space-x-1.5 pt-8 sm:space-x-1.5 sm:pt-0 md:justify-between md:space-x-2.5 xl:space-x-3 2xl:space-x-4">
+    <div className="mt-4 flex items-center justify-center space-x-1.5 pt-8 sm:space-x-1.5 sm:pt-0 md:justify-between md:space-x-2.5 xl:space-x-3 2xl:space-x-4">
       <div className="flex w-full flex-col items-center justify-center gap-y-0.5 rounded-lg bg-[#f6f7f9] px-1.5 py-2 sm:w-full sm:px-1.5 sm:py-1.5 md:w-full md:px-1.5 md:py-1.5 lg:w-full lg:px-2 xl:w-full xl:gap-y-1.5 xl:px-4 xl:py-3 2xl:w-full">
         <Calendar1Icon className="size-3 text-[var(--brand-theme-color)] md:size-4 lg:size-4 2xl:size-5" />
         <h2 className="text-[8px] font-medium text-[#282828] sm:text-[8px] lg:text-[10px] xl:text-[10px] 2xl:text-sm">
